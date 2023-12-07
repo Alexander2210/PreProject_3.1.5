0266dd6b1b945bf23378a24a67cbba34c88b0f1b
Alexander_Esin <alexavalon@yandex.ru>
2023-12-07 15:10:07 +0700

    done version_1.0.5

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,9 +1,11 @@
 INSERT INTO spring_security.users (name, surname, email, username, password)
 VALUES
-    ('Alexey', 'Vatin', 'vatalex@mail.ru', 'vatin', '$2a$12$e8h6wixiD9.3gAbNzVYHr.3wPGW3DJ8K7SInvIl/gBoqg1YycrsLausers_roles'),
-    ('Dmitriy', 'Ivanov', 'vatalex@mail.ru', 'ivanov', '$2a$12$vIwppZk/y8JKoQJLBynvt.RbR8uRo8TeuLMadnogfWkkUP47azjwC'),
-    ('Ivan', 'Sokolov', 'ivsokol@gmail.com', 'sokolov', '$2a$12$H27IWmu6ldCuo0bPgWWd8.jPNVo9S9eBWi07FflBCv0xg1PJbyaO.'),
-    ('Maria', 'Bunina', 'buninamar@mail.ru', 'bunina', '$2a$12$arP7UhgemlXLD2E3fM0U.OkLwfT8XZVCKQPwj312r3WZbqbbDOV.S');
+    ('Alexey', 'Vatin', 'vatalex@mail.ru', 'vatin', '$2a$12$cFQ1xnSjVjOWki2C8FkBN.PUeQwOtHV57/.oNCeIS8BF.jJwFYM66'),
+    ('Dmitriy', 'Ivanov', 'vatalex@mail.ru', 'ivanov', '$2a$12$cFQ1xnSjVjOWki2C8FkBN.PUeQwOtHV57/.oNCeIS8BF.jJwFYM66'),
+    ('Ivan', 'Sokolov', 'ivsokol@gmail.com', 'sokolov', '$2a$12$cFQ1xnSjVjOWki2C8FkBN.PUeQwOtHV57/.oNCeIS8BF.jJwFYM66'),
+    ('Maria', 'Bunina', 'buninamar@mail.ru', 'bunina', '$$2a$12$cFQ1xnSjVjOWki2C8FkBN.PUeQwOtHV57/.oNCeIS8BF.jJwFYM66');
+
+# Пароль для всех пользователей: 123
 
 INSERT INTO spring_security.roles (id, name)
 VALUES
